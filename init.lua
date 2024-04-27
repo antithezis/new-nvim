b703f273758e567321e3268b745bbc8228c1198d
antithezis <juandbm2001@gmail.com>
2024-04-27 11:54:03 -0500

    Remove config from init

--- init.lua
+++ init.lua
@@ -13,8 +13,3 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
---vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#3B4252", fg = "#5E81AC" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3B4252" })
---vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#3B4252" })
---vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "#3B4252" })
