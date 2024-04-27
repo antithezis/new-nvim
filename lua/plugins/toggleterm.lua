93138042a00cd74720973de844c9863793fea7ad
antithezis <juandbm2001@gmail.com>
2024-04-26 21:15:18 -0500

    float toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,13 +11,11 @@ return {
 			shading_factor = 1,
 			start_in_insert = true,
 			persist_size = true,
-			direction = "horizontal",
+			direction = "float",
 			close_on_exit = true,
 			shell = vim.o.shell,
 			float_opts = {
-				border = "single",
-				width = 200,
-				height = 50,
+				border = "curved",
 				winblend = 3,
 				highlights = {
 					border = "Normal",
@@ -25,6 +23,6 @@ return {
 				},
 			},
 		})
-		vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", {})
+		vim.keymap.set("n", "<C-/>", ":ToggleTerm<CR>", {})
 	end,
 }
