dd5450f0fc771e1c1a8fba1bf9798ea99cd5d269
antithezis <juandbm2001@gmail.com>
2024-04-27 11:53:31 -0500

    Configure clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -15,4 +15,8 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Config clipboard
+vim.opt.clipboard = "unnamedplus"
 
