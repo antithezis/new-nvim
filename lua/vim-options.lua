1a568af0138e0fd629eda7fdd149a4f5ca77b750
antithezis <juandbm2001@gmail.com>
2024-04-29 21:02:07 -0500

    Add keymaps to navigate between buffers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -27,3 +27,17 @@ vim.keymap.set(
   '<Esc>:%bdelete|edit #|normal`"<Return>',
   { desc = "Delete other buffers but the current one" }
 )
+
+-- Navigate between buffers --
+vim.keymap.set(
+  "n",
+  "<Tab>",
+  ":bnext<CR>",
+  { desc = "Navigate to the next buffer" }
+)
+vim.keymap.set(
+  "n",
+  "<S-Tab>",
+  ":bprevious<CR>",
+  { desc = "Navigate to the previous buffer" }
+)
