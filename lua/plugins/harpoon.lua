f80a4cad5c123a941a07145792803d07c297c348
antithezis <juandbm2001@gmail.com>
2024-04-29 22:01:31 -0500

    Change harpoon keymaps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		local harpoon = require("harpoon")
 		harpoon:setup()
-		vim.keymap.set("n", "<leader>a", function()
+		vim.keymap.set("n", "<C-a>", function()
 			harpoon:list():add()
 		end, { desc = "Add harpoon mark" })
 
@@ -15,19 +15,19 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<M-h>", function()
+		vim.keymap.set("n", "<C-h>", function()
 			harpoon:list():select(1)
 		end)
 
-		vim.keymap.set("n", "<M-j>", function()
+		vim.keymap.set("n", "<C-j>", function()
 			harpoon:list():select(2)
 		end)
 
-		vim.keymap.set("n", "<M-k>", function()
+		vim.keymap.set("n", "<C-k>", function()
 			harpoon:list():select(3)
 		end)
 
-		vim.keymap.set("n", "<M-l>", function()
+		vim.keymap.set("n", "<C-l>", function()
 			harpoon:list():select(4)
 		end)
 	end,
