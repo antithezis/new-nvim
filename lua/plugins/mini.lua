b012c513dafc68e1ad5b34a778bbb5f6245c1ee5
antithezis <juandbm2001@gmail.com>
2024-04-29 21:08:31 -0500

    Formating plugin files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,21 +1,20 @@
 return {
-  {
-    "echasnovski/mini.surround",
-    config = function()
-      require("mini.surround").setup({})
-    end,
-  },
-  {
-    "echasnovski/mini.pairs",
-    config = function()
-      require("mini.pairs").setup({})
-    end,
-  },
-  {
-    "echasnovski/mini.comment",
-    config = function()
-      require("mini.comment").setup({})
-    end,
-  }
+	{
+		"echasnovski/mini.surround",
+		config = function()
+			require("mini.surround").setup({})
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		config = function()
+			require("mini.pairs").setup({})
+		end,
+	},
+	{
+		"echasnovski/mini.comment",
+		config = function()
+			require("mini.comment").setup({})
+		end,
+	},
 }
-
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,12 +8,12 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 vim.wo.relativenumber = true
 
@@ -22,22 +22,12 @@ vim.opt.clipboard = "unnamedplus"
 
 -- Delete all buffers but the current one --
 vim.keymap.set(
-  "n",
-  "<leader>bq",
-  '<Esc>:%bdelete|edit #|normal`"<Return>',
-  { desc = "Delete other buffers but the current one" }
+	"n",
+	"<leader>bq",
+	'<Esc>:%bdelete|edit #|normal`"<Return>',
+	{ desc = "Delete other buffers but the current one" }
 )
 
 -- Navigate between buffers --
-vim.keymap.set(
-  "n",
-  "<Tab>",
-  ":bnext<CR>",
-  { desc = "Navigate to the next buffer" }
-)
-vim.keymap.set(
-  "n",
-  "<S-Tab>",
-  ":bprevious<CR>",
-  { desc = "Navigate to the previous buffer" }
-)
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Navigate to the next buffer" })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Navigate to the previous buffer" })
