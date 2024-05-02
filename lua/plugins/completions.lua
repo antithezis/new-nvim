9733711aa709e576c3b5dcc9375eadfe277de511
antithezis <juandbm2001@gmail.com>
2024-05-02 16:16:51 -0500

    Move lsp config to completion.lua file

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,4 +1,5 @@
 return {
+  ------CMP
 	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
@@ -16,7 +17,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-      local icons = require("utils.utils").icons
+			local icons = require("utils.utils").icons
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -29,13 +30,13 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            vim_item.kind = icons[vim_item.kind]
-            return vim_item
-          end,
-        },
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						vim_item.kind = icons[vim_item.kind]
+						return vim_item
+					end,
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -50,6 +51,49 @@ return {
 					{ name = "buffer" },
 				}),
 			})
+		end,
+	},
+  ------MASON / LSP
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+				"angularls",
+				"cssls",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }
