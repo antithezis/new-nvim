cd4c34a5fd17f12703f3151be154984d8b97955f
antithezis <juandbm2001@gmail.com>
2024-05-20 17:01:30 +0000

    Add keymaps to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,7 +18,10 @@ return {
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
+      vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end)
+      vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
       require("telescope").load_extension("ui-select")
     end,
   },
