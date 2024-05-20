6378a3b7f91b4f8555d1ea0ed3ed6c8e520e8aa6
antithezis <juandbm2001@gmail.com>
2024-05-20 19:17:35 +0000

    Add C-c to esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@ vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', '<Esc>', ':noh<CR>')
+vim.keymap.set('n', '<Esc>', ':noh<CR>', { silent = true })
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set( "n", "<leader>bo", ':%bd|e#|bd#<CR>', { silent = true })
