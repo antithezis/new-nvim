a0614ffde9640064496db80613820340d51e5c43
antithezis <juandbm2001@gmail.com>
2024-05-20 19:09:42 +0000

    Move to default keymaps

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -50,12 +50,9 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
+    config = function ()
+      vim.keymap.set("n", "s", function() require("flash").jump() end, {})
+      vim.keymap.set("n", "S", function() require("flash").treesitter() end, {})
+    end,
   }
 }
