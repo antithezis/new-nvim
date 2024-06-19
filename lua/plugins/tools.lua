e5d9506cf4303be36c7a48aac07cdd1151d99128
antithezis <juandbm2001@gmail.com>
2024-06-19 15:49:33 -0500

    Remove unused plugins

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -15,48 +15,5 @@ return {
     config = function()
       require("nvim-ts-autotag").setup()
     end,
-  },
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "Notes",
-          path = "~/Documents/Obsidian Vault/",
-        },
-      },
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-        new_notes_location = "current_dir",
-        prepend_note_id = true,
-        prepend_note_path = false,
-      },
-    },
-
-    {
-      "vhyrro/luarocks.nvim",
-      priority = 1000,
-      config = true,
-      opts = {
-        rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-      },
-    },
-    {
-      "rest-nvim/rest.nvim",
-      ft = "http",
-      dependencies = { "luarocks.nvim" },
-      config = function()
-        require("rest-nvim").setup()
-        vim.keymap.set("n", "<leader>rr", ":Rest run <CR>")
-        vim.keymap.set("n", "<leader>rl", ":Rest run last <CR>")
-      end,
-    },
-  },
+  }
 }
