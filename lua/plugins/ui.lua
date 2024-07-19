9c56cd8f9235d1ac139feb0ae364488113244cad
antithezis <juandbm2001@gmail.com>
2024-07-18 23:35:56 -0500

    Remove comments

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,7 +45,7 @@ return {
     dependencies = {
       "folke/persistence.nvim",
     },
-    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    lazy = false,
     opts = function()
       local logo = [[
      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠶⠀⠀⠀⠀⠀⣈⣿⣦⠀⠀⠀⠀
