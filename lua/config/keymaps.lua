8c8382532fd4d64b666c750ed06832fce64e631b
antithezis <juandbm2001@gmail.com>
2024-07-24 00:26:05 -0500

    Remove 'zv' from the keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,8 +6,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
