5107d7d0541156940c52b8d898f85f74c06ed1c1
antithezis <juandbm2001@gmail.com>
2024-07-23 22:11:02 -0500

    Refactor and add some options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,28 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set background=dark")
-
 vim.g.mapleader = " "
 vim.g.background = "light"
-vim.opt.swapfile = false
+
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
+
+vim.opt.swapfile = false
 vim.opt.foldmethod = "manual"
 vim.opt.fillchars = { eob = " " }
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- vim.opt.undotree = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append {"@-@"}
+
+vim.updatetime = 50
+-- vim.opt.colorcolumn = "80"
