e7e2fc248d99d836bbed4730da56dac952c00e3c
antithezis <juandbm2001@gmail.com>
2024-07-24 00:25:21 -0500

    Remove nohl autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -55,34 +55,9 @@ autocmd("FileType", {
   end,
 })
 
--- local hl_ns = vim.api.nvim_create_namespace('search')
--- local hlsearch_group = vim.api.nvim_create_augroup('hlsearch_group', { clear = true })
---
--- local function manage_hlsearch(char)
---   local key = vim.fn.keytrans(char)
---   local keys = { '<CR>', 'n', 'N', '*', '#', '?', '/', 'z' }
---
---   if vim.fn.mode() == 'n' then
---     if not vim.tbl_contains(keys, key) then
---       vim.cmd([[ :set nohlsearch ]])
---     elseif vim.tbl_contains(keys, key) then
---       vim.cmd([[ :set hlsearch ]])
---     end
---   end
---
---   vim.on_key(nil, hl_ns)
--- end
---
--- vim.api.nvim_create_autocmd('CursorMoved', {
---   group = hlsearch_group,
---   callback = function()
---     vim.on_key(manage_hlsearch, hl_ns)
---   end,
--- })
-
 vim.on_key(function(char)
   if vim.fn.mode() == "n" then
-    local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/", "z", "v" }, vim.fn.keytrans(char))
+    local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/", "z" }, vim.fn.keytrans(char))
     if vim.opt.hlsearch:get() ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
   end
 end, vim.api.nvim_create_namespace "auto_hlsearch")
