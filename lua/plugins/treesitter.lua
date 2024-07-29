45d82b6f2768e9eaf2de3966faef81e0b45f0423
antithezis <juandbm2001@gmail.com>
2024-07-29 16:09:47 -0500

    Add events an order autoinstall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   {
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
@@ -7,20 +9,28 @@ return {
       "nvim-treesitter/nvim-treesitter-context",
       "nvim-treesitter/playground",
     },
-    build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
         ensure_installed = {
-          "typescript",
-          "javascript",
-          "markdown",
-          "angular",
           "lua",
+          "angular",
+          "markdown",
+          "javascript",
+          "typescript",
         },
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<C-u>",
+          },
+        },
       })
     end
   }
