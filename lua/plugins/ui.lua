083931ff0d8cae50b942053070e6b44587a5ee37
antithezis <juandbm2001@gmail.com>
2024-07-29 16:09:13 -0500

    Add a possible useful plugin config (wilder)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,59 @@
 return {
   { "echasnovski/mini.indentscope", opts = {} },
+  -- {
+  --   "gelguy/wilder.nvim",
+  --   keys = { ":", "/", "?", },
+  --   config = function()
+  --     local wilder = require("wilder")
+  --
+  --     -- Create a highlight group for the popup menu
+  --     local text_highlight =
+  --         wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground =  "withe", background = "#1e1e2e" } })
+  --     local mauve_highlight =
+  --         wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = "#d2a8ff", background = "#1e1e2e"} })
+  --
+  --     -- Enable wilder when pressing :, / or ?
+  --     wilder.setup({ modes = { ":", "/", "?" } })
+  --
+  --     -- Enable fuzzy matching for commands and buffers
+  --     wilder.set_option("pipeline", {
+  --       wilder.branch(
+  --         wilder.cmdline_pipeline({
+  --           fuzzy = 1,
+  --         }),
+  --         wilder.vim_search_pipeline({
+  --           fuzzy = 1,
+  --         })
+  --       ),
+  --     })
+  --
+  --     wilder.set_option(
+  --       "renderer",
+  --       wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+  --         highlighter = wilder.basic_highlighter(),
+  --         highlights = {
+  --           default = text_highlight,
+  --           border = mauve_highlight,
+  --           accent = mauve_highlight,
+  --         },
+  --         pumblend = 5,
+  --         min_width = "100%",
+  --         min_height = "25%",
+  --         max_height = "25%",
+  --         border = "rounded",
+  --         left = { " ", wilder.popupmenu_devicons() },
+  --         right = { " ", wilder.popupmenu_scrollbar() },
+  --       }))
+  --     )
+  --   end,
+  -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup {}
+    end
+  },
   {
     "neanias/everforest-nvim",
     enabled = true,
