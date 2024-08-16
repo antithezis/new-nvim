76da1cd341f59327580240b456791d0faa55e9d6
antithezis <juandbm2001@gmail.com>
2024-08-16 15:33:23 -0500

    Add Dadbod options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,11 +21,67 @@ vim.opt.undofile = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 12
 -- vim.opt.signcolumn = "yes"
 -- vim.opt.isfname:append {"@-@"}
 
 vim.updatetime = 50
 
-vim.g.mkdp_browser = "edge"
-vim.g.mkdp_auto_start = 1
+local italic_group = {
+  -- "Comment",
+  "Keyword",
+}
+
+for _, group in ipairs(italic_group) do
+  vim.api.nvim_command("highlight " .. group .. " cterm=italic gui=italic")
+end
+
+vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_show_database_icon = 1
+vim.g.db_ui_force_echo_notifications = 1
+vim.g.db_ui_win_position = 'right'
+vim.g.db_ui_winwidth = 80
+
+vim.g.db_ui_table_helpers = {
+    mysql = {
+        Count = "select count(1) from {optional_schema}{table}",
+        Explain = "EXPLAIN {last_query}"
+    },
+    sqlite = {
+        Describe = "PRAGMA table_info({table})"
+    }
+}
+
+vim.g.db_ui_icons = {
+    expanded = {
+        db = '▾ ',
+        buffers = '▾ ',
+        saved_queries = '▾ ',
+        schemas = '▾ ',
+        schema = '▾ פּ',
+        tables = '▾ 藺',
+        table = '▾ ',
+    },
+    collapsed = {
+        db = '▸ ',
+        buffers = '▸ ',
+        saved_queries = '▸ ',
+        schemas = '▸ ',
+        schema = '▸ פּ',
+        tables = '▸ 藺',
+        table = '▸ ',
+    },
+    saved_query = '',
+    new_query = '璘',
+    tables = '離',
+    buffers = '﬘',
+    add_connection = '',
+    connection_ok = '✓',
+    connection_error = '✕',
+}
+
+-- opening it in a new tab
+vim.keymap.set('n', '<leader><leader>db', ':tab DBUI<cr>', {})
+
+-- just close the tab, but context related of the keybinding
+vim.keymap.set('n', '<leader><leader>tq', ':tabclose<cr>')
